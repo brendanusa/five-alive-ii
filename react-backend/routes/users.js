17d0eced531ac4bf1080ec37cadb07d199d7ed54
Brendan Bansavage <brendaniscool@gmail.com>
2017-12-29 15:14:12 -0500

    connect client, server, and db - using sample data

--- react-backend/routes/users.js
+++ react-backend/routes/users.js
@@ -1,16 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
+var pg = require('pg');
+var conString = "postgres://bbansavage:America1@localhost:5432/five_alive";
+
+var client = new pg.Client(conString);
+client.connect();
+
+let rows;
+
+console.log('clienttype', typeof client)
+
+// client.query("INSERT INTO test(stuff) values('hello');")
+
+// var query = client.query("SELECT * FROM test");
+
+// query.on('row', function(row) {
+//     console.log(row);
+// });
+
+// query.on('end', function() {
+//     client.end();
+// });
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+  // db.connect()
+
   // res.send('respond with a resource');
-  res.json([{
-    id: 1,
-    username: "samsepi0l"
-  }, {
-    id: 2,
-    username: "D0loresH4ze"
-  }]);
+
+  client.query("SELECT * FROM test;")
+    .then(values => {
+      console.log('done!', values.rows[0].stuff)
+      return res.json([{
+        id: 1,
+        username: values.rows[0].stuff
+      }, {
+        id: 2,
+        username: "D0loresH4ze"
+      }]);
+    });
+
 });
 
 module.exports = router;
